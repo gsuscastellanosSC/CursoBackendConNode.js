004393694573e06c122993f89ea1cf88173cd0f9
SC <jesuscastellanospaez@hotmail.com>
2019-11-02 05:07:33 -0500

    Duplex y Transforms streams Ambos sirven para simplificar nuestro código:
    Duplex: implementa los métodos write y read a la vez.
    Transform: es similar a Duplex pero con una sintaxis más corta.
    
    Clase#11 https://platzi.com/clases/1646-backend-nodejs/22020-duplex-y-transforms-streams/

---
+++ learning-node/streams/duplex.js
@@ -0,0 +1,17 @@
+const {Duplex} = require('stream');
+const duplexStream = new Duplex({
+    write(chunk, encoding, callback){
+        console.log(chunk.toString());
+        callback();
+    },
+    read(size){
+        if(this.currentCharCode > 90){
+            this.push(null);
+            return;
+        }
+        this.push(String.fromCharCode(this.currentCharCode++));
+    }
+});
+
+duplexStream.currentCharCode = 64;
+process.stdin.pipe(duplexStream).pipe(process.stdout);
