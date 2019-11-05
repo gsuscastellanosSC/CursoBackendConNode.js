8ddb2111a73021beb6888fad6555d4eda14c3d5e
SC <jesuscastellanospaez@hotmail.com>
2019-11-05 06:10:10 -0500

    Solución de reto

--- learning-node/movies-api/index.js
+++ learning-node/movies-api/index.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-undef */
 /* eslint-disable no-console */
 /* eslint-disable func-names */
 const express = require('express');
@@ -6,9 +7,35 @@ const app = express();
 
 const { config } = require('./config/index');
 
-app.get('/', (req, res) => {
-    res.send('hello world');
-});
+
+const bisiesto = (year) => {
+    aux=0;
+    if (year%4 ===0){
+        aux=-1;
+        if(year%100 ===0){
+            aux=0;
+        if (year%400 === 0){
+            aux=-1;
+        }
+        }else{
+            aux=-1;
+        }
+    }else{
+        aux=0;
+    }
+   
+    if (aux===0){
+        year=0;
+    }if (aux===-1) {
+        year=-1
+    }
+    return (year === -1) ?'El año es bisiesto':'El año no es bisiesto';
+  }
+  
+  app.get('/bisiesto/:year', function(req, res) {
+    let message = bisiesto(req.params.year);
+    res.send(message);
+  });
 
 app.get('/json', (req, res) => {
     res.json({hello: 'world'});
